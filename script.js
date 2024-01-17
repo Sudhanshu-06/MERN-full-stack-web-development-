9ff7743b6e383e3bca7bb28b039450b87cd44f9a
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-01-17 08:40:55 +0530

    add

---
+++ script.js
@@ -0,0 +1,29 @@
+const a=10*15;
+console.log(a);
+
+let age=18;
+if(age>18){
+    console.log("you are eligible");
+}else{
+    console.log("you are not eligible");
+}
+
+// Switch
+let ch=2;
+switch(ch){
+    case 1:
+        console.log("I am one");
+        break;
+    case 2:
+        console.log("I am second");
+        break;
+    default :
+        console.log("Not match");
+
+}
+
+//Looping
+
+for(let i=1; i<=8; i++){
+    console.log(i);
+}
