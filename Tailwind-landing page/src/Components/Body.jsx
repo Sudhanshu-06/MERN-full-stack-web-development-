5b16d51b00f26111128641b1c8aafd10d7512fea
Sudhanshu-06 <sudhanshu767778@gmail.com>
2024-05-25 22:53:14 +0530

    push

--- Tailwind-landing page/src/Components/Body.jsx
+++ Tailwind-landing page/src/Components/Body.jsx
@@ -2,16 +2,24 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div>
-        <div className='flex item-center justify-center'>
-        <img src="./public/assets/Blue-Shape.svg" alt="1st" className='-rotate-45 h-64'/>
-        <img src="./public/assets/Pink-Shape.svg" alt="2nd" className='absolute -rotate-[30deg] h-64' />
-        <img src="./public/assets/Purple-Shape.svg" alt="3rd" className='absolute -rotate-[15deg] h-64' />
-        <img src="./public/assets/Hero-Model.png" alt="4tth" className='absolute' h-64 />
+    <div className='space-y-4 lg:flex'>
+        <div className='flex item-center justify-center lg:flex-1 lg:order-2 lg:justify-end'>
+        <img src="./public/assets/Blue-Shape.svg" alt="1st" className='-rotate-45 h-64 md:h-72 lg:h-[400px]'/>
+        <img src="./public/assets/Pink-Shape.svg" alt="2nd" className='absolute -rotate-[30deg] h-64 md:h-72 lg:h-[400px]' />
+        <img src="./public/assets/Purple-Shape.svg" alt="3rd" className='absolute -rotate-[15deg] h-64 md:h-72 lg:h-[400px]' />
+        <img src="./public/assets/Hero-Model.png" alt="4tth" className='absolute h-64 md:h-72 lg:h-[400px]' />
         </div>
-        <div>
-            <h1>Host your website in less than 5 minutes</h1>
-            <p>with Hoster,get youe</p>
+        <div className='lg:flex-1 lg:order-1'>
+            <h1 className='text-5xl font-bold font-playfair leading-tight'>Host your website in less than 5 minutes</h1>
+            <p className='font-lato  text-gray-400'>with Hoster,get your website in no less than 5 minutes with the most competitive pricing packages available online.</p>
+            <form action="" className='flex flex-col gap-4 md:flex-row'>
+                <input className='rounded-md px-4 py-3 placeholder:text-gray-400' type="email"  placeholder='Enter email address'/>
+                <button className='rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-600'>Join Waitlist</button>
+            </form>
+            <div className='flex gap-2 pt-6'>
+                <img src="./public/assets/Checkmark.svg" alt="checkmark" />
+                <p className='font-lato text-gray-400'>No spam,ever Unsubscribe anytime.</p>
+            </div>
         </div>
     </div>
   )
--- Tailwind-landing page/src/Components/Header.jsx
+++ Tailwind-landing page/src/Components/Header.jsx
@@ -8,7 +8,16 @@ const Header = () => {
             <img src="./public/assets/logo.svg" alt="logo" />
             <button className='bg-gradient-to-r from-orange-400 to-red-400 text-xs text-white px-2.5 py-1.5 rounded-2xl'>Hotstar is hiring</button>
         </div>
-        <div>
+        <ul className='hidden lg:flex justify-between items-center font-lato text-gray-400 gap-6'>
+            <li><a href="#">Plans</a></li>
+            <li><a href="#">Find Domain</a></li>
+            <li> <a href="#">Why Hoster</a></li>
+        </ul>
+        <div className='hidden lg:flex justify-center items-center font-lato gap-6'>
+            <a className='text-gray-400 ' href="#">Sign in</a>
+            <button className='rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-600'>Join Waitlist</button>
+        </div>
+        <div className='lg:hidden'>
         <FaBars /> 
         </div>
     </div>
